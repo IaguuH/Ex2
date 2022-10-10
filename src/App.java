3f93e5d4ad8f2a81e673842e182aacd856bca3d2
Iago Gomes <iagomg420@gmail.com>
2022-10-10 20:30:38 -0300

    Projeto Java

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+    public class App {
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+     System.out.print("Numero - ");
+     int numero = teclado.nextInt();
+
+     System.out.println("O numero informado foi " + numero + "");
+
+    }
+}
